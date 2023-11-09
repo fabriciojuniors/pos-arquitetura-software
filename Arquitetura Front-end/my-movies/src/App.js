a47ef27e0f05411703a2da65f8c0ac676dd933da
Junior Souza <32983889+fabriciojuniors@users.noreply.github.com>
2023-11-08 21:32:41 -0300

    Componentes de estilo utilizando styled-components

--- Arquitetura Front-end/my-movies/src/App.js
+++ Arquitetura Front-end/my-movies/src/App.js
@@ -1,14 +1,15 @@
 import { Header } from './components/Header';
 import { MovieList } from './components/MovieList';
+import { MoviesContainer } from './styles/MoviesContainer';
 
 function App() {
   return (
     <div className="App">
       <Header />
 
-      <section>
+      <MoviesContainer>
         <MovieList movies={[]} />
-      </section>
+      </MoviesContainer>
     </div>
   );
 }
